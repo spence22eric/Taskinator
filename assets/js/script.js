db208d374e2a1a67314b6597d242a682edb22a97
Eric Spencer <spencer22eric@gmail.com>
2022-02-22 22:24:49 -0500

    add innerhtml for select menu and task name

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,26 @@ let createTaskHandler = function (event) {
 
     event.preventDefault();
 
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
 };
 
 
